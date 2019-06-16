086dbee8d89e42446648734f7c05b8d61e46a97a
AndreySomov <AndreySomov1993@gmail.com>
2019-06-16 20:48:01 +0300

    feat: add filex

---
+++ precache-manifest.2846405da6870fe1fda8aba13e47c0e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818952247e2d5e46b5cb8bd0ab7e4687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f805b59205c3c55752",
+    "url": "/static/css/main.064ec83d.chunk.css"
+  },
+  {
+    "revision": "2760c9f0a79d3c06e923",
+    "url": "/static/js/2.8b7d72f7.chunk.js"
+  },
+  {
+    "revision": "85f805b59205c3c55752",
+    "url": "/static/js/main.0ad931f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
